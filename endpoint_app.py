7cebca55cd0b737e73444408f05146e9a60087b9
Pablo Rosa <pablo.julian.rosa@gmail.com>
2025-07-29 10:12:14 -0300

    add port

--- endpoint_app.py
+++ endpoint_app.py
@@ -196,5 +196,6 @@ def shopify_to_bigquery():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
